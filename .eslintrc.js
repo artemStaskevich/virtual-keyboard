2fad5d7848f8826b374921606d5b718c85ce5e5f
Artem <artem-git@clinicbuddy.com>
2020-04-04 22:21:07 +0300

    feat: add task solution

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+	env: {
+	  browser: true,
+	  node: true,
+	},
+	extends: ['eslint-config-airbnb-base'],
+	rules: {
+	  'linebreak-style': ['error', 'windows'],
+	  quotes: ['error', 'single', { allowTemplateLiterals: true }],
+	  semi: ['error', 'never'],
+	  'no-console': 'off',
+	  'no-use-before-define': ['error', { functions: false, variables: false }],
+	  'operator-linebreak': [2, 'before'],
+	  'arrow-parens': [2, 'as-needed'],
+	  'no-restricted-syntax': ['error', 'WithStatement', "BinaryExpression[operator='in']"],
+	  'no-unused-expressions': [2, { allowTernary: true }],
+	  'import/extensions': [2, 'ignorePackages', { js: 'always' }],
+	  'object-curly-newline': ['error', { multiline: true }],
+	},
+ }
---
+++ script.js
@@ -0,0 +1,1006 @@
+const keysArray = []
+keysArray[0] = [{
+  className: 'Backquote',
+  classWide: 'normal',
+  en: {
+    caseDown: '`',
+    caseUp: '~',
+  },
+  ru: {
+    caseDown: 'ё',
+    caseUp: 'Ё',
+  },
+}, {
+  className: 'Digit1',
+  classWide: 'normal',
+  en: {
+    caseDown: '1',
+    caseUp: '!',
+  },
+  ru: {
+    caseDown: '1',
+    caseUp: '!',
+  },
+}, {
+  className: 'Digit2',
+  classWide: 'normal',
+  en: {
+    caseDown: '2',
+    caseUp: '@',
+  },
+  ru: {
+    caseDown: '2',
+    caseUp: '"',
+  },
+}, {
+  className: 'Digit3',
+  classWide: 'normal',
+  en: {
+    caseDown: '3',
+    caseUp: '#',
+  },
+  ru: {
+    caseDown: '3',
+    caseUp: '№',
+  },
+}, {
+  className: 'Digit4',
+  classWide: 'normal',
+  en: {
+    caseDown: '4',
+    caseUp: '$',
+  },
+  ru: {
+    caseDown: '4',
+    caseUp: ';',
+  },
+}, {
+  className: 'Digit5',
+  classWide: 'normal',
+  en: {
+    caseDown: '5',
+    caseUp: '%',
+  },
+  ru: {
+    caseDown: '5',
+    caseUp: '%',
+  },
+}, {
+  className: 'Digit6',
+  classWide: 'normal',
+  en: {
+    caseDown: '6',
+    caseUp: '^',
+  },
+  ru: {
+    caseDown: '6',
+    caseUp: ':',
+  },
+}, {
+  className: 'Digit7',
+  classWide: 'normal',
+  en: {
+    caseDown: '7',
+    caseUp: '&',
+  },
+  ru: {
+    caseDown: '7',
+    caseUp: '?',
+  },
+}, {
+  className: 'Digit8',
+  classWide: 'normal',
+  en: {
+    caseDown: '8',
+    caseUp: '*',
+  },
+  ru: {
+    caseDown: '8',
+    caseUp: '*',
+  },
+}, {
+  className: 'Digit9',
+  classWide: 'normal',
+  en: {
+    caseDown: '9',
+    caseUp: '(',
+  },
+  ru: {
+    caseDown: '9',
+    caseUp: '(',
+  },
+}, {
+  className: 'Digit0',
+  classWide: 'normal',
+  en: {
+    caseDown: '0',
+    caseUp: ')',
+  },
+  ru: {
+    caseDown: '0',
+    caseUp: ')',
+  },
+}, {
+  className: 'Minus',
+  classWide: 'normal',
+  en: {
+    caseDown: '-',
+    caseUp: '_',
+  },
+  ru: {
+    caseDown: '-',
+    caseUp: '_',
+  },
+}, {
+  className: 'Equal',
+  classWide: 'normal',
+  en: {
+    caseDown: '=',
+    caseUp: '+',
+  },
+  ru: {
+    caseDown: '=',
+    caseUp: '+',
+  },
+}, {
+  className: 'Backspace',
+  classWide: 'extra',
+  en: {
+    caseDown: 'Backspace',
+    caseUp: 'Backspace',
+  },
+  ru: {
+    caseDown: 'Backspace',
+    caseUp: 'Backspace',
+  },
+}],
+keysArray[1] = [{
+  className: 'Tab',
+  classWide: 'normal',
+  en: {
+    caseDown: 'Tab',
+    caseUp: 'Tab',
+  },
+  ru: {
+    caseDown: 'Tab',
+    caseUp: 'Tab',
+  },
+}, {
+  className: 'KeyQ',
+  classWide: 'normal',
+  en: {
+    caseDown: 'q',
+    caseUp: 'Q',
+  },
+  ru: {
+    caseDown: 'й',
+    caseUp: 'Й',
+  },
+}, {
+  className: 'KeyW',
+  classWide: 'normal',
+  en: {
+    caseDown: 'w',
+    caseUp: 'W',
+  },
+  ru: {
+    caseDown: 'ц',
+    caseUp: 'Ц',
+  },
+}, {
+  className: 'KeyE',
+  classWide: 'normal',
+  en: {
+    caseDown: 'e',
+    caseUp: 'E',
+  },
+  ru: {
+    caseDown: 'у',
+    caseUp: 'У',
+  },
+}, {
+  className: 'KeyR',
+  classWide: 'normal',
+  en: {
+    caseDown: 'r',
+    caseUp: 'R',
+  },
+  ru: {
+    caseDown: 'к',
+    caseUp: 'К',
+  },
+}, {
+  className: 'KeyT',
+  classWide: 'normal',
+  en: {
+    caseDown: 't',
+    caseUp: 'T',
+  },
+  ru: {
+    caseDown: 'е',
+    caseUp: 'Е',
+  },
+}, {
+  className: 'KeyY',
+  classWide: 'normal',
+  en: {
+    caseDown: 'y',
+    caseUp: 'Y',
+  },
+  ru: {
+    caseDown: 'н',
+    caseUp: 'Н',
+  },
+}, {
+  className: 'KeyU',
+  classWide: 'normal',
+  en: {
+    caseDown: 'u',
+    caseUp: 'U',
+  },
+  ru: {
+    caseDown: 'г',
+    caseUp: 'Г',
+  },
+}, {
+  className: 'KeyI',
+  classWide: 'normal',
+  en: {
+    caseDown: 'i',
+    caseUp: 'I',
+  },
+  ru: {
+    caseDown: 'ш',
+    caseUp: 'Ш',
+  },
+}, {
+  className: 'KeyO',
+  classWide: 'normal',
+  en: {
+    caseDown: 'o',
+    caseUp: 'O',
+  },
+  ru: {
+    caseDown: 'щ',
+    caseUp: 'Щ',
+  },
+}, {
+  className: 'KeyP',
+  classWide: 'normal',
+  en: {
+    caseDown: 'p',
+    caseUp: 'P',
+  },
+  ru: {
+    caseDown: 'з',
+    caseUp: 'З',
+  },
+}, {
+  className: 'BracketLeft',
+  classWide: 'normal',
+  en: {
+    caseDown: '[',
+    caseUp: '{',
+  },
+  ru: {
+    caseDown: 'х',
+    caseUp: 'Х',
+  },
+}, {
+  className: 'BracketRight',
+  classWide: 'normal',
+  en: {
+    caseDown: ']',
+    caseUp: '}',
+  },
+  ru: {
+    caseDown: 'ъ',
+    caseUp: 'Ъ',
+  },
+}, {
+  className: 'Backslash',
+  classWide: 'middle',
+  en: {
+    caseDown: '\\',
+    caseUp: '|',
+  },
+  ru: {
+    caseDown: '\\',
+    caseUp: '/',
+  },
+}, {
+  className: 'Delete',
+  classWide: 'normal',
+  en: {
+    caseDown: 'Del',
+    caseUp: 'Del',
+  },
+  ru: {
+    caseDown: 'Del',
+    caseUp: 'Del',
+  },
+}],
+keysArray[2] = [{
+  className: 'CapsLock',
+  classWide: 'middle',
+  en: {
+    caseDown: 'CapsLock',
+    caseUp: 'CapsLock',
+  },
+  ru: {
+    caseDown: 'CapsLock',
+    caseUp: 'CapsLock',
+  },
+}, {
+  className: 'KeyA',
+  classWide: 'normal',
+  en: {
+    caseDown: 'a',
+    caseUp: 'A',
+  },
+  ru: {
+    caseDown: 'ф',
+    caseUp: 'Ф',
+  },
+}, {
+  className: 'KeyS',
+  classWide: 'normal',
+  en: {
+    caseDown: 's',
+    caseUp: 'S',
+  },
+  ru: {
+    caseDown: 'ы',
+    caseUp: 'Ы',
+  },
+}, {
+  className: 'KeyD',
+  classWide: 'normal',
+  en: {
+    caseDown: 'd',
+    caseUp: 'D',
+  },
+  ru: {
+    caseDown: 'в',
+    caseUp: 'В',
+  },
+}, {
+  className: 'KeyF',
+  classWide: 'normal',
+  en: {
+    caseDown: 'f',
+    caseUp: 'F',
+  },
+  ru: {
+    caseDown: 'а',
+    caseUp: 'А',
+  },
+}, {
+  className: 'KeyG',
+  classWide: 'normal',
+  en: {
+    caseDown: 'g',
+    caseUp: 'G',
+  },
+  ru: {
+    caseDown: 'п',
+    caseUp: 'П',
+  },
+}, {
+  className: 'KeyH',
+  classWide: 'normal',
+  en: {
+    caseDown: 'h',
+    caseUp: 'H',
+  },
+  ru: {
+    caseDown: 'р',
+    caseUp: 'Р',
+  },
+}, {
+  className: 'KeyJ',
+  classWide: 'normal',
+  en: {
+    caseDown: 'j',
+    caseUp: 'J',
+  },
+  ru: {
+    caseDown: 'о',
+    caseUp: 'О',
+  },
+}, {
+  className: 'KeyK',
+  classWide: 'normal',
+  en: {
+    caseDown: 'k',
+    caseUp: 'K',
+  },
+  ru: {
+    caseDown: 'л',
+    caseUp: 'Л',
+  },
+}, {
+  className: 'KeyL',
+  classWide: 'normal',
+  en: {
+    caseDown: 'l',
+    caseUp: 'L',
+  },
+  ru: {
+    caseDown: 'д',
+    caseUp: 'Д',
+  },
+}, {
+  className: 'Semicolon',
+  classWide: 'normal',
+  en: {
+    caseDown: ';',
+    caseUp: ':',
+  },
+  ru: {
+    caseDown: 'ж',
+    caseUp: 'Ж',
+  },
+}, {
+  className: 'Quote',
+  classWide: 'normal',
+  en: {
+    caseDown: '\'',
+    caseUp: '|',
+  },
+  ru: {
+    caseDown: 'э',
+    caseUp: 'Э­',
+  },
+}, {
+  className: 'Enter',
+  classWide: 'extra',
+  en: {
+    caseDown: 'Enter',
+    caseUp: 'Enter',
+  },
+  ru: {
+    caseDown: 'Enter',
+    caseUp: 'Enter',
+  },
+}],
+keysArray[3] = [{
+  className: 'ShiftLeft',
+  classWide: 'extra',
+  en: {
+    caseDown: 'Shift',
+    caseUp: 'Shift',
+  },
+  ru: {
+    caseDown: 'Shift',
+    caseUp: 'Shift',
+  },
+}, {
+  className: 'KeyZ',
+  classWide: 'normal',
+  en: {
+    caseDown: 'z',
+    caseUp: 'Z',
+  },
+  ru: {
+    caseDown: 'я',
+    caseUp: 'Я',
+  },
+}, {
+  className: 'KeyX',
+  classWide: 'normal',
+  en: {
+    caseDown: 'x',
+    caseUp: 'X',
+  },
+  ru: {
+    caseDown: 'ч',
+    caseUp: 'Ч',
+  },
+}, {
+  className: 'KeyC',
+  classWide: 'normal',
+  en: {
+    caseDown: 'c',
+    caseUp: 'C',
+  },
+  ru: {
+    caseDown: 'с',
+    caseUp: 'С',
+  },
+}, {
+  className: 'KeyV',
+  classWide: 'normal',
+  en: {
+    caseDown: 'v',
+    caseUp: 'V',
+  },
+  ru: {
+    caseDown: 'м',
+    caseUp: 'М',
+  },
+}, {
+  className: 'KeyB',
+  classWide: 'normal',
+  en: {
+    caseDown: 'b',
+    caseUp: 'B',
+  },
+  ru: {
+    caseDown: 'и',
+    caseUp: 'И',
+  },
+}, {
+  className: 'KeyN',
+  classWide: 'normal',
+  en: {
+    caseDown: 'n',
+    caseUp: 'N',
+  },
+  ru: {
+    caseDown: 'т',
+    caseUp: 'Т',
+  },
+}, {
+  className: 'KeyM',
+  classWide: 'normal',
+  en: {
+    caseDown: 'm',
+    caseUp: 'M',
+  },
+  ru: {
+    caseDown: 'ь',
+    caseUp: 'Ь',
+  },
+}, {
+  className: 'Comma',
+  classWide: 'normal',
+  en: {
+    caseDown: ',',
+    caseUp: '<',
+  },
+  ru: {
+    caseDown: 'б',
+    caseUp: 'Б',
+  },
+}, {
+  className: 'Period',
+  classWide: 'normal',
+  en: {
+    caseDown: '.',
+    caseUp: '>',
+  },
+  ru: {
+    caseDown: 'ю',
+    caseUp: 'Ю',
+  },
+}, {
+  className: 'Slash',
+  classWide: 'normal',
+  en: {
+    caseDown: '/',
+    caseUp: '?',
+  },
+  ru: {
+    caseDown: '.',
+    caseUp: ',',
+  },
+}, {
+  className: 'ArrowUp',
+  classWide: 'normal',
+  en: {
+    caseDown: '&#8593',
+    caseUp: '&#8593',
+  },
+  ru: {
+    caseDown: '&#8593',
+    caseUp: '&#8593',
+  },
+}, {
+  className: 'ShiftRight',
+  classWide: 'extra',
+  en: {
+    caseDown: 'Shift',
+    caseUp: 'Shift',
+  },
+  ru: {
+    caseDown: 'Shift',
+    caseUp: 'Shift',
+  },
+}],
+keysArray[4] = [{
+  className: 'ControlLeft',
+  classWide: 'middle',
+  en: {
+    caseDown: 'Ctrl',
+    caseUp: 'Ctrl',
+  },
+  ru: {
+    caseDown: 'Ctrl',
+    caseUp: 'Ctrl',
+  },
+}, {
+  className: 'MetaLeft',
+  classWide: 'middle',
+  en: {
+    caseDown: 'Win',
+    caseUp: 'Win',
+  },
+  ru: {
+    caseDown: 'Win',
+    caseUp: 'Win',
+  },
+}, {
+  className: 'AltLeft',
+  classWide: 'middle',
+  en: {
+    caseDown: 'Alt',
+    caseUp: 'Alt',
+  },
+  ru: {
+    caseDown: 'Alt',
+    caseUp: 'Alt',
+  },
+}, {
+  className: 'Space',
+  classWide: 'space',
+  en: {
+    caseDown: ' ',
+    caseUp: ' ',
+  },
+  ru: {
+    caseDown: ' ',
+    caseUp: ' ',
+  },
+}, {
+  className: 'AltRight',
+  classWide: 'middle',
+  en: {
+    caseDown: 'Alt',
+    caseUp: 'Alt',
+  },
+  ru: {
+    caseDown: 'Alt',
+    caseUp: 'Alt',
+  },
+}, {
+  className: 'ArrowLeft',
+  classWide: 'normal',
+  en: {
+    caseDown: '&#8592',
+    caseUp: '&#8592',
+  },
+  ru: {
+    caseDown: '&#8592',
+    caseUp: '&#8592',
+  },
+}, {
+  className: 'ArrowDown',
+  classWide: 'normal',
+  en: {
+    caseDown: '&#8595',
+    caseUp: '&#8595',
+  },
+  ru: {
+    caseDown: '&#8595',
+    caseUp: '&#8595',
+  },
+}, {
+  className: 'ArrowRight',
+  classWide: 'normal',
+  en: {
+    caseDown: '&#8594',
+    caseUp: '&#8594',
+  },
+  ru: {
+    caseDown: '&#8594',
+    caseUp: '&#8594',
+  },
+}, {
+  className: 'ControlRight',
+  classWide: 'middle',
+  en: {
+    caseDown: 'Ctrl',
+    caseUp: 'Ctrl',
+  },
+  ru: {
+    caseDown: 'Ctrl',
+    caseUp: 'Ctrl',
+  },
+}]
+
+const data = {
+  specials: ['Backspace', 'Tab', 'Enter', 'CapsLock', 'ShiftLeft', 'ShiftRight', 'Delete', 'AltLeft', 'AltRight', 'ControlLeft', 'ControlRight', 'MetaLeft'],
+  keyboardRows: keysArray,
+}
+
+class VirtualKeyboard {
+  constructor() {
+    this.isCapsLock = false
+    this.isShiftLeft = false
+    this.isShiftRight = false
+    this.isDoubleShift = false
+    this.case = 'caseDown'
+    this.lang = 'en'
+    this.textarea = document.createElement('textarea')
+    this.keyboard = document.createElement('div')
+    this.info = document.createElement('div')
+    this.current = {
+      element: null,
+      code: null,
+      event: null,
+      char: null,
+    }
+  }
+
+  init(a) {
+    this.initInfo()
+    this.initKeyboard(a)
+    this.initLanguage()
+
+    document.addEventListener('keyup', this.keyUpHandler.bind(this))
+    document.addEventListener('keydown', this.keyDownHandler.bind(this))
+    this.keyboard.addEventListener('mouseup', this.mouseUpHandler.bind(this))
+    this.keyboard.addEventListener('mousedown', this.mouseDownHandler.bind(this))
+  }
+
+  initKeyboard(a) {
+    const container = document.createElement('div')
+
+    container.classList.add('container')
+    this.textarea.classList.add('text')
+    this.keyboard.classList.add('keyboard')
+
+    this.keyboard.append(this.renderKeybord(a))
+    container.append(this.textarea, this.keyboard, this.info)
+    document.body.append(container)
+  }
+
+  initInfo() {
+    const langInfo = document.createElement('p')
+    langInfo.textContent = 'To change language press - "Ctrl" + "Alt"'
+
+    const sysInfo = document.createElement('p')
+    sysInfo.textContent = 'Made in Windows OS'
+
+    this.info.append(langInfo, sysInfo)
+  }
+
+  initLanguage() {
+    if (localStorage.lang === 'ru') {
+      this.changeLang()
+    }
+  }
+
+  renderKeybord(a) {
+    const elements = document.createDocumentFragment()
+    for (let i = 0; i < a.length; i++) {
+      const row = document.createElement('div')
+      row.classList.add('row')
+      for (let k = 0; k < a[i].length; k++) {
+        const keyDiv = document.createElement('div')
+        keyDiv.classList.add('keyboard-key', 'key', a[i][k].classWide, a[i][k].className)
+
+        const elemRu = document.createElement('span')
+        elemRu.classList.add('ru', 'hidden')
+        elemRu.insertAdjacentHTML('afterBegin', `<span class='caseDown hidden'>${a[i][k].ru.caseDown}</span>`)
+        elemRu.insertAdjacentHTML('beforeEnd', `<span class='caseUp hidden'>${a[i][k].ru.caseUp}</span>`)
+        keyDiv.appendChild(elemRu)
+
+        const elemEn = document.createElement('span')
+        elemEn.classList.add('en')
+        elemEn.insertAdjacentHTML('afterBegin', `<span class='caseDown'>${a[i][k].en.caseDown}</span>`)
+        elemEn.insertAdjacentHTML('beforeEnd', `<span class='caseUp hidden'>${a[i][k].en.caseUp}</span>`)
+        keyDiv.appendChild(elemEn)
+
+        row.appendChild(keyDiv)
+      }
+
+      elements.appendChild(row)
+    }
+    return elements
+  }
+
+  addActiveState() {
+    this.current.element.classList.add('active')
+  }
+
+  removeActiveState() {
+    this.current.element.classList.remove('active')
+  }
+
+  changeCase() {
+    this.changeHidden()
+
+    if (this.case == 'caseDown') {
+      this.case = 'caseUp'
+    } else {
+      this.case = 'caseDown'
+    }
+
+    this.changeHidden()
+  }
+
+  changeLang() {
+    this.changeHidden()
+
+    if (this.lang == 'en') {
+      this.lang = 'ru'
+    } else {
+      this.lang = 'en'
+    }
+
+    this.changeHidden()
+    localStorage.setItem('lang', this.lang)
+  }
+
+  changeHidden() {
+    const e = this.keyboard.querySelectorAll(`div>.${this.lang}`)
+    for (let i = 0; i < e.length; i++) {
+      e[i].classList.toggle('hidden')
+      e[i].querySelectorAll(`span.${this.case}`)[0].classList.toggle('hidden')
+    }
+  }
+
+  addCharHandler() {
+    const e = this.textarea.value
+    const position = this.textarea.selectionStart
+    if (data.specials.includes(this.current.code)) {
+      switch (this.current.code) {
+        case 'Backspace':
+          if (position > 0 && position <= e.length) {
+            this.removeCharFromTextArea(e, position - 1)
+          }
+          break
+        case 'Delete':
+          if (position >= 0 && position <= e.length - 1) {
+            this.removeCharFromTextArea(e, position)
+          }
+          break
+        case 'Tab':
+          this.current.char = '	'
+          this.addCharToTextArea(e, position)
+          break
+        case 'Enter':
+          this.current.char = '\n'
+          this.addCharToTextArea(e, position)
+          break
+        case 'CapsLock':
+          if (this.isCapsLock) {
+            this.isCapsLock = false
+            this.removeActiveState()
+            this.changeCase()
+          } else {
+            this.isCapsLock = true
+            this.addActiveState()
+            this.changeCase()
+          }
+          break
+        case 'ShiftLeft':
+          if (!this.isShiftRight) {
+            this.changeCase()
+          } else {
+            this.isDoubleShift = true
+          }
+          break
+        case 'ShiftRight':
+          if (!this.isShiftLeft) {
+            this.changeCase()
+          } else {
+            this.isDoubleShift = true
+          }
+      }
+    } else {
+      this.addCharToTextArea(e, position)
+    }
+
+    if (this.current.event.ctrlKey && this.current.event.altKey) {
+      this.changeLang()
+    }
+  }
+
+  addCharToTextArea(e, position) {
+    if (position >= 0 && position <= e.length) {
+      this.textarea.value = e.slice(0, position) + this.current.char + e.slice(position, e.length),
+      this.textarea.selectionStart = position + this.current.char.length
+      this.textarea.selectionEnd = position + this.current.char.length
+    } else {
+      this.textarea.value += this.current.char
+    }
+  }
+
+  removeCharFromTextArea(e, position) {
+    e = e.slice(0, position) + e.slice(position + 1, e.length)
+    this.textarea.value = e
+    this.textarea.selectionStart = position
+    this.textarea.selectionEnd = position
+  }
+
+  keyUpHandler(e) {
+    const el = document.getElementsByClassName(e.code)[0]
+    this.current.element = el.closest('div')
+
+    if (this.current.code !== 'CapsLock') {
+      this.removeActiveState()
+    }
+
+    if (e.code === 'ShiftLeft') {
+      this.isShiftLeft = false
+      this.changeCase()
+      this.removeActiveState()
+      if (this.isDoubleShift) {
+        this.current.element = document.querySelector('.ShiftRight')
+        this.removeActiveState()
+        this.isShiftRight = false
+        this.isDoubleShift = false
+      }
+    }
+    if (e.code === 'ShiftRight') {
+      this.isShiftRight = false
+      this.changeCase()
+      this.removeActiveState()
+      if (this.isDoubleShift) {
+        this.current.element = document.querySelector('.ShiftLeft')
+        this.removeActiveState()
+        this.isShiftLeft = false
+        this.isDoubleShift = false
+      }
+    }
+  }
+
+  keyDownHandler(e) {
+    e.preventDefault()
+    this.current.event = e
+    this.current.code = e.code;
+    [this.current.element] = document.getElementsByClassName(e.code)
+
+    if (this.current.code === 'ShiftLeft') {
+      this.isShiftLeft = true
+    }
+    if (this.current.code === 'ShiftRight') {
+      this.isShiftRight = true
+    }
+
+    this.addActiveState()
+    this.current.char = this.current.element.querySelectorAll('span:not(.hidden)')[1].textContent
+    this.addCharHandler()
+  }
+
+  mouseUpHandler(e) {
+    this.current.event = e
+    this.current.element = e.target.closest('div')
+    this.current.code = this.current.element.classList[3]
+
+    if (this.current.element && this.current.element.classList.contains('key') && this.current.code !== 'CapsLock') {
+      this.removeActiveState()
+    }
+
+    if (this.current.code === 'ShiftLeft') {
+      this.isShiftLeft = false
+      this.changeCase()
+      this.removeActiveState()
+    }
+    if (this.current.code === 'ShiftRight') {
+      this.isShiftRight = false
+      this.changeCase()
+      this.removeActiveState()
+    }
+  }
+
+  mouseDownHandler(e) {
+    this.current.event = e
+    this.current.element = e.target.closest('div')
+
+    if (this.current.element.classList.contains('key')) {
+      this.current.char = this.current.element.querySelectorAll('span:not(.hidden)')[1].textContent
+      this.current.code = this.current.element.classList[3]
+      this.addActiveState()
+      this.addCharHandler()
+    }
+  }
+}
+
+window.onload = () => {
+  const VKeyboard = new VirtualKeyboard()
+  VKeyboard.init(data.keyboardRows)
+}
